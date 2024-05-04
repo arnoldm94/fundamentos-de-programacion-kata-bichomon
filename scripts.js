ed447759000159931ccde1ece529ea4b45780805
Arnold Melet <arnoldmelet18@gmail.com>
2024-05-04 13:50:18 +0200

    falta premium

--- scripts.js
+++ scripts.js
@@ -1,27 +1,40 @@
-/* 
-
-
-
-Imprime por consola la URL de la página.
-
-Imprime por consola el dominio de la página.
-
-Imprime todos los nodos de imagen.
-
-Sustituye el atributo "src" de todas las imágenes por este "https://media.giphy.com/media/2v170e71aanfi/giphy.gif"
-
-Premium:
-
-Cambia el fondo de todos los infocard-lg-data text-muted para todos los Pokimon voladores itype flying */
-
 console.log(document.title);
 
 //Cambia el título "Generation 1 Pokémon" por "Generasión 1 Pokimon".
 const generacion = document.getElementById ("gen-1")
 generacion.innerHTML = "Generasión 1 Pokimon"
 
-//Cambia el color de fondo de la primera generación de Pokimon.
-/* 
-const fondo = document.getElementsByClassName ("infocard ").style.backgroundcolor =  "#ff0000"
 
+//Cambia el color de fondo de la primera generación de Pokimon.
+generacion.setAttribute ("style", "background-color: #ff0000;")
+
+
+/* 
+Imprime por consola la URL de la página. */
+    var urlActual=document.URL;
+    console.log("La url es: " + urlActual);
+
+
+    
+/* Imprime por consola el dominio de la página. */
+    function dominio() {
+        var loc = window.location;
+        var pathName = loc.pathname.substring(0, loc.pathname.lastIndexOf('/') + 1);
+        return loc.href.substring(0, loc.href.length - ((loc.pathname + loc.search + loc.hash).length - pathName.length));
+    };
+    console.log("el dominio es: ", dominio());
+
+
+    /* Imprime todos los nodos de imagen. */
+        const img = document.querySelectorAll ("img");
+    console.log (img); 
+
+/*     Sustituye el atributo "src" de todas las imágenes por este "https://media.giphy.com/media/2v170e71aanfi/giphy.gif" */
+ img.forEach(img => {
+    img.src ="https://media.giphy.com/media/2v170e71aanfi/giphy.gif"
+});
+    
+
+/* Cambia el fondo de todos los infocard-lg-data text-muted para todos los Pokimon voladores itype flying 
  */
+
